a56f309df5555833758be2e23a6fd30003a0e5a7
benjamin <adolphe906@gmail.com>
2022-07-24 15:17:34 +0200

    Déclaration premières constantes

---
+++ src/com/badolphe/utils/Constantes.java
@@ -0,0 +1,9 @@
+package com.badolphe.utils;
+
+public class Constantes {
+    public static int CONVERT_MS_TO_SEC = 1000;
+    public static String EMPTY_CASE = "NULL";
+    public static int CASE_PER_LINE = 9;
+    public static int QUINE_MIN = 0;
+    public static int QUINE_MAX = 99;
+}
